db834f149c9c3d584c54c6dd253af7a089306728
Henri1979 <henri.h1979@gmail.com>
2020-05-05 10:39:35 +0000

    added fruit and drawfunction to fruit

--- draw.js
+++ draw.js
@@ -3,13 +3,18 @@ const ctx = canvas.getContext("2d");
 const scale = 10;
 const rows = canvas.height / scale;
 const columns = canvas.width / scale;
+
 var snake;
 
 (function setup() {
 snake = new Snake();
+fruit = new Fruit();
+fruit.pickLocation();
+
 
 window.setInterval(() => {
-ctx.clearRect(0, 0, canvas.width, canvas.height);    
+ctx.clearRect(0, 0, canvas.width, canvas.height); 
+fruit.draw();  
 snake.update();
 snake.draw();
 }, 250);
